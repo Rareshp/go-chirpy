369dceba94d5cfdc8dc0bb667c47af4347f63ebc
Rareshp <plesearares@protonmail.com>
2024-04-05 14:23:33 +0300

    database; also separeted files

--- main.go
+++ main.go
@@ -1,33 +1,21 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"regexp"
 )
 
 const filepathRoot = "."
 const port = "8080"
 const chirpCharLimit = 140
+const dbPath = "database.json"
 
 type apiConfig struct { 
   fileserverHits int
+  DB             *DB
 }
-type body struct {
-  Body string `json:"body"`
-}
-type errorReply struct {
-  Error string `json:"error"`
-}
-type validReply struct {
-  Valid bool `json:"valid"`
-}
-type cleanReply struct {
-  CleanedBody string `json:"cleaned_body"`
-}
+
 func middlewareCors(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
@@ -53,70 +41,6 @@ func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
   fmt.Fprintf(w, "<html><body><h1>Welcome, Chirpy Admin</h1><p>Chirpy has been visited %d times!</p></body></html>", cfg.fileserverHits)
 }
 
-func jsonReplies(w http.ResponseWriter, jsonReply errorReply, status int) {
-    eJsonReply, err := json.Marshal(jsonReply)
-    if err != nil {
-      log.Printf("Error marshalling JSON: %s", err)
-      w.WriteHeader(500)
-      return
-    }
-    w.WriteHeader(status)
-    w.Write(eJsonReply)
-}
-
-func replaceProfane(bod *body) {
-    newBody := bod.Body
-    profanities := [3]string{"kerfuffle", "sharbert", "fornax"}
-    
-    for _, pw := range profanities {
-        re := regexp.MustCompile(`(?i)\b` + pw + `\b`)
-        newBody = re.ReplaceAllString(newBody, "****")
-    }
-    
-    bod.Body = newBody
-}
-
-func (cfg *apiConfig) handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-  
-  decoder := json.NewDecoder(r.Body)
-  bod := body{}
-  decodingErr := decoder.Decode(&bod)
-
-  if decodingErr != nil {
-    jsonReply := errorReply{
-      Error: "Something went wrong",
-    }
-    log.Printf(jsonReply.Error)
-
-    jsonReplies(w, jsonReply, 500)
-    return
-  }
-
-  if (len(bod.Body) > chirpCharLimit) {
-    jsonReply := errorReply{
-      Error: "Chirp is too long",
-    }
-    log.Printf(jsonReply.Error)
-
-    jsonReplies(w, jsonReply, 400)
-    return
-  }
-
-  replaceProfane(&bod)
-  clean := cleanReply {
-    CleanedBody: bod.Body,
-  }
-
-  validJsonReply, err := json.Marshal(clean)
-  if err != nil {
-    log.Printf("Error marshalling JSON: %s", err)
-    w.WriteHeader(500)
-  }
-  // response 200 is implied
-  w.Write(validJsonReply)
-}
-
 func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		cfg.fileserverHits++
@@ -133,8 +57,15 @@ func healthHandler(w http.ResponseWriter, _ *http.Request) {
 func main() {
   mux := http.NewServeMux() 
 
+
+	db, err := NewDB("database.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+
   apiCfg := apiConfig {
     fileserverHits: 0,
+    DB: db,
   }
 
   // or http.Dir("./app")
@@ -142,7 +73,9 @@ func main() {
 	mux.HandleFunc("GET /api/healthz", healthHandler)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
 	mux.HandleFunc("GET /api/reset", apiCfg.handlerReset)
-  mux.HandleFunc("POST /api/validate_chirp", apiCfg.handlerValidateChirp)
+
+	mux.HandleFunc("POST /api/chirps", apiCfg.handlerChirpsCreate)
+	mux.HandleFunc("GET /api/chirps", apiCfg.handlerChirpsRetrieve)
 
   // wrap the mux to add CORS 
   corsMux := middlewareCors(mux)
